2fed62487028fc1be45ed64c8bb974e193069e49
변찬우 <chan00106@naver.com>
2022-01-20 18:02:13 +0900

    Add 글자 수 제한

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,53 @@
+import { CloseRounded, MoreHorizRounded } from "@mui/icons-material";
+import { Avatar, IconButton } from "@mui/material";
+import { useState } from "react";
+import { useRecoilState } from "recoil";
+import { modalState } from "../atoms/modelAtom";
+
 /* eslint-disable @next/next/no-img-element */
-function Post({ post }) {
+function Post({ post, modalPost }) {
+  const [modalOpen, setModalOpen] = useRecoilState(modalState);
+  const [showInput, setShowInput] = useState(false);
+
+  const truncate = (string, n) =>
+    string?.length > n ? string.substr(0, n - 1) + " ...see more" : string;
+
   return (
-    <div>
-      <img src={post.photoUrl} alt="postImg" />
-      <p>{post.input}</p>
+    <div
+      className={`bg-white dark:bg-[#1d2226] ${
+        modalPost ? "rounded-r-lg" : "rounded-lg"
+      } space-y-2 py-2.5 border border-gray-300 dark:border-none`}
+    >
+      <div className="flex items-center px-2.5 cursor-pointer">
+        <Avatar src={post.userImg} className="!h-10 !w-10 cursor-pointer" />
+        <div className="mr-auto ml-2 leading-none">
+          <h6 className="font-medium hover:text-blue-500 hover:underline">
+            {post.username}
+          </h6>
+          <p className="text-sm dark:text-white//75 opacity-80">{post.email}</p>
+        </div>
+        {modalPost ? (
+          <IconButton onClick={() => setModalOpen(false)}>
+            <CloseRounded className="dark:text-white/75 h-7 w-7" />
+          </IconButton>
+        ) : (
+          <IconButton>
+            <MoreHorizRounded className="dark:text-white/75 h-7 w-7" />
+          </IconButton>
+        )}
+      </div>
+
+      {post.input && (
+        <div className="px-2.5 break-all md:break-normal">
+          {modalPost || showInput ? (
+            <p onClick={() => setShowInput(false)}>{post.input}</p>
+          ) : (
+            <p onClick={() => setShowInput(true)}>
+              {truncate(post.input, 150)}
+            </p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
